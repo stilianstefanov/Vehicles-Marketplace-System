5de8ffaaa7149081a7ccc5fb1fa5d7b7caea8999
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-07-03 10:50:10 +0300

    Created custom UserController

---
+++ ThinkElectric.Web/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThinkElectric.Web.Controllers;
+
+public class UserController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
