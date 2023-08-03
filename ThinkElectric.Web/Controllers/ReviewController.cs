b81e56c904a8d0f4b838b0a334c994ab8f9104a7
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-08-03 12:46:14 +0300

    Removed help comments, Created ReviewController

---
+++ ThinkElectric.Web/Controllers/ReviewController.cs
@@ -0,0 +1,19 @@
+﻿namespace ThinkElectric.Web.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+public class ReviewController : Controller
+{
+    private readonly IReviewService _reviewService;
+
+    public ReviewController(IReviewService reviewService)
+    {
+        _reviewService = reviewService;
+    }
+
+    public IActionResult AddToProduct()
+    {
+        return View();
+    }
+}
