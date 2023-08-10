f745ed7ce88f1365181707a4ac2ee305bfcab829
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-08-10 18:50:40 +0300

    Created Test project for services

---
+++ ThinkElectric.Services.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ThinkElectric.Services.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
